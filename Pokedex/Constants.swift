d27838a9b31beb965794665ef200872a7a15530d
J0na17anT98 <pilotj@outlook.com>
2016-08-01 10:01:31 -0400

    parsed data from pokeAPI
    parsed data

---
+++ Pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Pokedex
+//
+//  Created by Jonathan Tsistinas on 7/28/16.
+//  Copyright © 2016 Techinator. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "api/v2/pokemon/"
+
+typealias DownloadComplete = () -> ()
--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -12,28 +12,40 @@ class PokemonDetailVC: UIViewController {
     
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         nameLbl.text = pokemon.name
+        mainImg.image = UIImage(named: "\(pokemon.pokedexId)")
         
+        pokemon.downloadPokemonDetails { () -> () in
+            // this will be called after download is done
+        }
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
+    
+    
+    
 }
